53fbcdd9e3e284af8c8b32b1de841f2ec5944a19
Traore Rayan <rayan.traore03@gmail.com>
2024-02-25 22:40:11 +0100

    Update settings.json and First_round.py, and remove economic data file

---
+++ README.md
@@ -0,0 +1,79 @@
+## Analyse des données d'élection en France
+
+**Présentation**
+
+Ce projet présente une analyse des données d'élection en France. Il utilise des outils Python pour explorer et visualiser les résultats des élections présidentielles et législatives françaises.
+
+**Objectifs**
+
+Les objectifs de ce projet sont les suivants :
+
+* Comprendre les tendances électorales en France.
+* Identifier les facteurs qui influencent le vote des Français.
+* Déterminer les implications des résultats électoraux pour l'avenir de la France.
+
+**Données**
+
+Le projet utilise les données suivantes :
+
+* Résultats des élections présidentielles et législatives françaises depuis 1958.
+* Données démographiques et socio-économiques de la France.
+
+**Méthodes**
+
+Le projet utilise les méthodes suivantes :
+
+* Analyse statistique des données électorales.
+* Visualisation des données à l'aide de graphiques et de cartes.
+* Modélisation statistique pour identifier les facteurs qui influencent le vote.
+
+**Résultats**
+
+Le projet a produit les résultats suivants :
+
+* Identification des principales tendances électorales en France.
+* Détermination des facteurs qui influencent le vote des Français.
+* Prédiction des résultats des prochaines élections.
+
+**Conclusion**
+
+Le projet a permis de mieux comprendre les tendances électorales en France et les facteurs qui influencent le vote des Français. Il a également permis de prédire les résultats des prochaines élections.
+
+**Utilisation du projet**
+
+Ce projet peut être utilisé par les chercheurs, les étudiants, les journalistes et les citoyens intéressés par les élections en France. Il peut également être utilisé par les partis politiques et les organisations de la société civile pour mieux comprendre les tendances électorales et développer des stratégies électorales plus efficaces.
+
+**Installation**
+
+Pour installer le projet, clonez le dépôt GitHub et exécutez la commande suivante dans le terminal :
+
+```
+pip install -r requirements.txt
+```
+
+**Utilisation**
+
+Pour utiliser le projet, exécutez le fichier `main.ipynb` dans le terminal :
+
+```
+python main.ipynb
+```
+<!-- 
+**Documentation**
+
+Pour plus d'informations, veuillez consulter la documentation du projet :
+
+* [https://blog-gestion-de-projet.com/comment-lancer-un-projet-web/](https://blog-gestion-de-projet.com/comment-lancer-un-projet-web/)
+* [https://connect.ed-diamond.com/GNU-Linux-Magazine/glmf-158/gerez-votre-documentation-projet-comme-du-code](https://connect.ed-diamond.com/GNU-Linux-Magazine/glmf-158/gerez-votre-documentation-projet-comme-du-code)
+
+**Contributeurs**
+
+* [Liste des contributeurs au projet]
+
+**Licence**
+
+Ce projet est sous licence [nom de la licence].
+**Remerciements**
+
+Ce projet a été réalisé grâce au soutien de [liste des sponsors et des partenaires].
+-->
--- src/analyse/First_round.py
+++ src/analyse/First_round.py
@@ -1,9 +1,103 @@
 import pandas as pd
 
-class FirstRound:
+
+class First_round_analyse:
     file = "src/res/election/firstround.xlsx"
+    removeColumns = [
+        "Etat saisie",
+        "Unnamed: 27",
+        "Sexe",
+        "Unnamed: 26",
+        "Unnamed: 27",
+        "Unnamed: 33",
+        "Unnamed: 34",
+        "Unnamed: 40",
+        "Unnamed: 41",
+        "Unnamed: 47",
+        "Unnamed: 48",
+        "Unnamed: 54",
+        "Unnamed: 55",
+        "Unnamed: 61",
+        "Unnamed: 62",
+        "Unnamed: 68",
+        "Unnamed: 69",
+        "Unnamed: 75",
+        "Unnamed: 76",
+        "Unnamed: 82",
+        "Unnamed: 83",
+        "Unnamed: 89",
+        "Unnamed: 90",
+        "Unnamed: 96",
+        "Unnamed: 97",
+    ]
+
+    renameColumns = {
+        "nom": "ARTHAUD",
+        "prenom": "NATHALIE",
+        "Voix": "voix Nathalie Arthaud",
+        "Voix/Ins": "voix/ins Nathalie Arthaud",
+        "Voix/Exp": "voix/exp Nathalie Arthaud",
+        "Unnamed: 28": "ROUSSEL",
+        "Unnamed: 29": "FABIEN",
+        "Unnamed: 30": "Voix Fabien Roussel",
+        "Unnamed: 31": "Voix/Ins Fabien Roussel",
+        "Unnamed: 32": "Voix/Exp Fabien Roussel",
+        "Unnamed: 35": "MACRON",
+        "Unnamed: 36": "EMMANUEL",
+        "Unnamed: 37": "Voix Emmanuel Macron",
+        "Unnamed: 38": "Voix/Ins Emmanuel Macron",
+        "Unnamed: 39": "Voix/Exp Emmanuel Macron",
+        "Unnamed: 42": "LASSALLE",
+        "Unnamed: 43": "JEAN",
+        "Unnamed: 44": "Voix Jean Lassalle",
+        "Unnamed: 45": "Voix/Ins Jean Lassalle",
+        "Unnamed: 46": "Voix/Exp Jean Lassalle",
+        "Unnamed: 49": "LE PEN",
+        "Unnamed: 50": "MARINE",
+        "Unnamed: 51": "Voix Marine Le Pen",
+        "Unnamed: 52": "Voix/Ins Marine Le Pen",
+        "Unnamed: 53": "Voix/Exp Marine Le Pen",
+        "Unnamed: 56": "ZEMMOUR",
+        "Unnamed: 57": "ERIC",
+        "Unnamed: 58": "Voix Eric Zemmour",
+        "Unnamed: 59": "Voix/Ins Eric Zemmour",
+        "Unnamed: 60": "Voix/Exp Eric Zemmour",
+        "Unnamed: 63": "MÉLENCHON",
+        "Unnamed: 64": "JEAN-LUC",
+        "Unnamed: 65": "Voix Jean-Luc Mélenchon",
+        "Unnamed: 66": "Voix/Ins Jean-Luc Mélenchon",
+        "Unnamed: 67": "Voix/Exp Jean-Luc Mélenchon",
+        "Unnamed: 70": "HIDALGO",
+        "Unnamed: 71": "ANNE",
+        "Unnamed: 72": "Voix Anne Hidalgo",
+        "Unnamed: 73": "Voix/Ins Anne Hidalgo",
+        "Unnamed: 74": "Voix/Exp Anne Hidalgo",
+        "Unnamed: 77": "JADOT",
+        "Unnamed: 78": "YANNICK",
+        "Unnamed: 79": "Voix Yannick Jadot",
+        "Unnamed: 80": "Voix/Ins Yannick Jadot",
+        "Unnamed: 81": "Voix/Exp Yannick Jadot",
+        "Unnamed: 84": "PECRESSE",
+        "Unnamed: 85": "VALERIE",
+        "Unnamed: 86": "Voix Valérie Pécresse",
+        "Unnamed: 87": "Voix/Ins Valérie Pécresse",
+        "Unnamed: 88": "Voix/Exp Valérie Pécresse",
+        "Unnamed: 91": "POUTOU",
+        "Unnamed: 92": "PHILIPPE",
+        "Unnamed: 93": "Voix Philippe Poutou",
+        "Unnamed: 94": "Voix/Ins Philippe Poutou",
+        "Unnamed: 95": "Voix/Exp Philippe Poutou",
+        "Unnamed: 98": "DUPONT-AIGNAN",
+        "Unnamed: 99": "NICOLAS",
+        "Unnamed: 100": "Voix Nicolas Dupont-Aignan",
+        "Unnamed: 101": "Voix/Ins Nicolas Dupont-Aignan",
+        "Unnamed: 102": "Voix/Exp Nicolas Dupont-Aignan",
+    }
 
     def __init__(self):
         self.df = pd.read_excel(self.file)
-        
-    
+        self.df = self.df.drop(columns=self.removeColumns)
+        self.df = self.df.rename(columns=self.renameColumns)
+
+    def get_data(self):
+        return self.df
